3f27cdba5482249dc759702961c1c074ee1471d5
Stefan <me@Stefan.com>
2019-07-28 15:44:40 +0300

    Finish core of framework

---
+++ app/controllers/Pages.php
@@ -0,0 +1,14 @@
+<?php 
+	class Pages {
+		public function __construct(){
+			
+		}
+
+		public function index(){
+			echo 'index page';
+		}
+
+		public function about(){
+			echo 'about page';
+		}
+	}
